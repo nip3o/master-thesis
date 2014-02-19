c00eed913ad8541017ab9dcec5e6082777bbe141
Niclas Olofsson <n@niclasolofsson.se>
2014-02-19 14:18:41 +0100

    Even niftier injection thingy

--- code/random/inject.rb
+++ code/random/inject.rb
@@ -1,15 +1,8 @@
 
-def suppress_warnings
-  original_verbosity = $VERBOSE
-  $VERBOSE = nil
-  result = yield
-  $VERBOSE = original_verbosity
-  return result
-end
-
-
 module Humm
   class Meep
+    attr_accessor :age
+
     def self.plusone_map_each(arr)
       arr.map { |e| e + 1 }
     end
@@ -17,28 +10,54 @@ module Humm
     def self.kaka(arr)
       plusone_map_each(arr + [100])
     end
+
+    def dog_age
+      @age * 7
+    end
   end
 end
 
 module Injector
-  def self.helper(callee, callr, *args)
-    puts "Method '#{callee}' called from #{callr} with args #{args.inspect}"
+  def self.helper(callee, callr, obj, ret, *args)
+    instance_vars = obj.instance_variables
+    values = instance_vars.map { |var| obj.instance_variable_get var }
+
+    inst = instance_vars.zip(values)
+    puts "Method '#{callee}' called from #{callr} with args #{args.inspect}, #{inst} => #{ret}"
+  end
+
+  def self.do_injection(real_klass, method)
+    old_method = real_klass.send(:instance_method, method)
+
+    real_klass.send(:define_method, method) do |*args|
+      ret = old_method.bind(self).(*args)
+      Injector::helper(__callee__, caller(), self, ret, *args)
+      ret
+    end
   end
 
   def self.inject(klass, method)
-    old_method_name = '_old_' + method
-    klass.singleton_class.send(:alias_method, old_method_name, method)
-    klass.singleton_class.send(:define_method, method) do |*args|
-      Injector::helper(__callee__, caller, *args)
-      self.send(old_method_name, *args)
+    if klass.instance_methods.include? method
+      self.do_injection(klass, method)
+    elsif klass.singleton_class.instance_methods.include? method
+      self.do_injection(klass.singleton_class, method)
+    else
+      raise ArgumentError, "Class '#{klass.name}' has no method '#{method}'"
     end
   end
 end
 
 p Humm::Meep.kaka([3, 4, 1])
-puts '#' * 40
+p Humm::Meep.new.age
+puts '#' * 80
 
-Injector.inject(Humm::Meep, 'kaka')
-Injector.inject(Humm::Meep, 'plusone_map_each')
+Injector.do_injection(Humm::Meep.singleton_class, :kaka)
+Injector.inject(Humm::Meep, :plusone_map_each)
+Injector.inject(Humm::Meep, :dog_age)
 
 p Humm::Meep.kaka([3, 4, 1])
+
+dog = Humm::Meep.new
+dog.age = 3
+
+p dog.dog_age
